f35fa7e5c071ebaf31a77c8a2648e84172a21ae6
Chris Skov Sørensen <chso0002@stud.kea.dk>
2025-01-03 23:08:44 +0100

    removed compareDistance() and made small adjustments.

--- src/core/pathfinding.js
+++ src/core/pathfinding.js
@@ -17,6 +17,7 @@ export function euclideanDistance(a, b) {
  * distance = |Δx| + |Δy|
  * @param {{row: number, col: number}} a coord a
  * @param {{row: number, col: number}} b coord b
+ * @returns {number} Manhattan distance between coords
  */
 export function manhattanDistance(a, b) {
   return Math.abs(a.row - b.row) + Math.abs(a.col - b.col);
@@ -44,11 +45,11 @@ export function aStar(start, goal, board, h) {
   fScore[getCoordKey(startCoords)] = h(startCoords, goalCoords);
 
   const cameFrom = {};
-
   const openSetCoords = new Set();
-  const openSet = new PriorityQueue((a, b) => compareDistance(a, b, fScore));
-  openSet.enqueue(startCoords);
-  openSetCoords.add(startCoords);
+  const openSet = new PriorityQueue();
+
+  openSet.enqueue(startCoords, fScore[getCoordKey(startCoords)]);
+  openSetCoords.add(getCoordKey(startCoords));
 
   while (openSet.size > 0) {
     let current = openSet.dequeue();
@@ -71,11 +72,13 @@ export function aStar(start, goal, board, h) {
       if (tentativeGScore < neighbourGScore) {
         cameFrom[neighbourKey] = current;
         gScore[neighbourKey] = tentativeGScore;
-        fScore[neighbourKey] = tentativeGScore + h(neighbour, goalCoords);
-        if (!openSetCoords.has(neighbour)) {
-          openSet.enqueue(neighbour);
+        const newFScore = tentativeGScore + h(neighbour, goalCoords);
+        fScore[neighbourKey] = newFScore;
+        if (!openSetCoords.has(neighbourKey)) {
+            openSet.enqueue(neighbour, newFScore);
+            openSetCoords.add(neighbourKey);
         }
-      }
+    }
     }
   }
   return [];
@@ -99,23 +102,9 @@ function reconstructPath(cameFrom, current) {
 }
 
 /**
- * Compares position a and b's distance to goal.
- * if a is closer to goal than b: returns positive number
- * if b is closer to goal than a: returns negative number
- * if they are the same distance returns 0
- * @param {{row: number, col: number}} a position a
- * @param {{row: number, col: number}} b position b
- * @param {{row: number, col: number}} goal position goal
- * @returns {number} comparison value between a and b's fScore
- */
-function compareDistance(a, b, fScore) {
-  return fScore[getCoordKey(b)] - fScore[getCoordKey(a)];
-}
-
-/**
  * @param {{row:number, col: number}} coords coords to get grid coord key for
- * @returns {string} unique key for the grid cell of a given position as a string. Example: row: 5, col: 7, would give key "57"
+ * @returns {string} unique key for the grid cell of a given position as a string. Example: row: 5, col: 7, would give key "5,7"
  */
 function getCoordKey(coords) {
-  return coords.row.toString() + coords.col.toString();
+  return `${coords.row},${coords.col}`;
 }
